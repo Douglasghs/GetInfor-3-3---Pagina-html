ea4ffef96100c7872b012133c1b0126e245f9a75
douglas ruan de araujo fernando <67982779+Douglasghs@users.noreply.github.com>
2021-10-21 22:05:08 -0300

    Terminando o front e começando o script
    Revisar a URL(GET) do service java para passar os parâmetros corretos

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,17 @@
-//alert("kkkkkkk")
+function Pesquisar(IPV4, IPV6){
+
+    if((IPV4 == "") && (IPV6 == "")){
+        alert("Preencha um dos campos disponíveis para digitação (IPV4 ou IPV6)");
+    }
+    else if((IPV4 != "") || (IPV6 != "")){
+
+    }
+
+    var url = "https://dicasdejavascript.com.br/exemplo.txt";//Sua URL
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, false);
+    xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
+    
+    console.log(xhttp.responseText);
+}
